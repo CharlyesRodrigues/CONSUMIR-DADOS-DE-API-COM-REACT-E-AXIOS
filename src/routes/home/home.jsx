a732075878247a7d2ab289ef44dc5ca1d07a8a2f
CharlyesRodrigues <charlyessouza@gmail.com>
2023-04-26 13:12:19 -0300

    Update de da API

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,48 +1,67 @@
- import axios from "axios";
+import axios from "axios";
 import "../home/style.css";
- 
- import React from 'react'
 
- import { useState, useEffect } from 'react';
+import React from 'react'
+
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
- 
- export const Home = ()=> {
+
+export const Home = () => {
 
 
-  const[posts,setPosts] = useState([]); 
+  const [posts, setPosts] = useState([]);
 
 
   // Função que resgatará os dados da API
   // console.log("testando"); Colocar dentro da função getPosts para testar o funcionamento da função no useEffect
-  const getPosts = async() => {
+  const getPosts = async () => {
     try {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
-        ); 
- 
-      console.log(response);
+      );
+
+      const data = response.data;
+      setPosts(data);
 
     } catch (error) {
       console.log(error);
     }
 
   }
-// Agora iremos fazer uma função para que a cada renderização ela executena quantidade que nós determinamos
-//},[])  esse array na última linha faz com que a função execute uma vez
+  // Agora iremos fazer uma função para que a cada renderização ela executena quantidade que nós determinamos
+  //},[])  esse array na última linha faz com que a função execute uma vez
 
-useEffect (()=>{
-  getPosts();
-
-
-  
-},[])
+  useEffect(() => {
+    getPosts();
 
 
-   return (
-     <div>home</div>
-   )
 
- }
- 
- 
+  }, [])
+
+
+  return (
+    <div className="container">
+<h1>Últimos Posts</h1>
+{posts.length === 0 ? <p>Carregando...</p> : (
+posts.map((posts)=>(
+
+
+<div className="posts"  key={posts.id}>
+<h2>{posts.title} </h2>
+<p>{posts.body} </p>
+<Link to={`/posts/${posts.id}`} className="btn" >Ler mais...</Link>
+
+</div>
+
+
+))
+
+
+)}
+
+    </div>
+  )
+
+}
+
